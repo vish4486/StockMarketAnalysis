e35f2ad3c1b5df8a095116fd6a48fad49b92fed8
Muhammad Mubashar Shahzad <167110909+mubasharshahzad64@users.noreply.github.com>
2025-04-01 01:02:46 -0700

    Errors FIXED

--- src/main/java/com/sdm/model/ModelScore.java
+++ src/main/java/com/sdm/model/ModelScore.java
@@ -9,10 +9,7 @@ public class ModelScore {
     public final double rmse;  
     public final double predictedPrice;
 
-
-
-
-    public ModelScore(String modelName, String timeframe, double rSquared, double mse, double rmse, double mae,double predictedPrice) {
+    public ModelScore(String modelName, String timeframe, double rSquared, double mse, double rmse, double mae, double predictedPrice) {
         this.modelName = modelName;
         this.timeframe = timeframe;
         this.rSquared = rSquared;
@@ -25,8 +22,7 @@ public class ModelScore {
     @Override
     public String toString() {
         return String.format("%s [%s] ➤ R²: %.4f | MSE: %.4f | RMSE: %.4f | MAE: %.4f | Predicted: %.2f",modelName, timeframe, rSquared, mse, rmse, mae, predictedPrice);
-        }
-    
+    }
     
     public double getPredictedPrice() {
         return predictedPrice;
